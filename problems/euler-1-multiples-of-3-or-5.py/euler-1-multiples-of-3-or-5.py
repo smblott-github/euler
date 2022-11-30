02b1dab5666da0f8505149aff5e99c45b8921415
Stephen Blott <stephen.blott@dcu.ie>
2022-11-30 08:58:41 +0000

    Note re. closed formula.

--- problems/euler-1-multiples-of-3-or-5.py/euler-1-multiples-of-3-or-5.py
+++ problems/euler-1-multiples-of-3-or-5.py/euler-1-multiples-of-3-or-5.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 
+# NOTE:
+#
+# The following solution iterates.  But I'm pretty sure that there's a closed
+# formula for this, and that the iteration is unnecessary.
+
 import sys
 
 N = int(sys.argv[1])
