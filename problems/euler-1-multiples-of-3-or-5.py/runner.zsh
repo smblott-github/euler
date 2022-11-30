baeee3676dbb9df223980d1a63fe7f92dc3d1ee9
Stephen Blott <stephen.blott@dcu.ie>
2022-11-30 07:18:13 +0000

    Try to set TASK environment variable for testing.

--- problems/euler-1-multiples-of-3-or-5.py/runner.zsh
+++ problems/euler-1-multiples-of-3-or-5.py/runner.zsh
@@ -1,9 +1,12 @@
 #!/usr/bin/env zsh
 
 #
-# The TASK environment variable will be set in the Einstein
-# execution environment.
+# The TASK environment variable will be set in the Einstein execution environment.
 #
+# Try to set the TASK environment variable for testing...
+#
+[[ -z $TASK ]] && [[ -f $PWD:t ]] && TASK=$PWD:t
+[[ -z $TASK ]] && [[ -f $PWD:h:t ]] && TASK=$PWD:h:t
 
 for v in 0 10 100 500 1000 5000 10000
 do
