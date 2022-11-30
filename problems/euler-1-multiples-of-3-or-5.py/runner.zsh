c8385c018d369283d7560e2197a405587419342e
Stephen Blott <stephen.blott@dcu.ie>
2022-11-30 07:22:57 +0000

    Exit on failure.

--- problems/euler-1-multiples-of-3-or-5.py/runner.zsh
+++ problems/euler-1-multiples-of-3-or-5.py/runner.zsh
@@ -8,6 +8,10 @@
 [[ -z $TASK ]] && [[ -f $PWD:t ]] && TASK=$PWD:t
 [[ -z $TASK ]] && [[ -f $PWD:h:t ]] && TASK=$PWD:h:t
 
+# Fail (exit) immediately if any of the following commands fail.
+#
+set -e
+
 # The show-exec-command is available in the Einstein execution environment.
 # It is also available in the `bin` directory in the project repo.  You can
 # install it locally from there for testing.
