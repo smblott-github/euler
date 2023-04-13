2f4169494de958d76378cf4a4f39e7474012190f
Lucas Moritel <lucas.moritel@epitech.eu>
2023-04-13 14:12:17 +0100

    fix test

---
+++ problems/euler-11-largest-product-in-a-grid.cpp/test-3/runner.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# The TASK environment variable will be set automatically in the Einstein execution
+# environment.
+#
+# For testing, try to set the TASK environment variable...
+#
+[ -z "$TASK" ] && [ -f "$PWD:t" ] && TASK="$PWD:t"
+[ -z "$TASK" ] && [ -f "$PWD:h:t" ] && TASK="$PWD:h:t"
+
+# Fail (exit) immediately if any of the following commands fail.
+#
+set -e
+
+# Compile the program
+make -C ../.
+
+# Execute the program with the specified grid.txt file
+if [ -f grid.txt ]; then
+    ../euler-11-largest-product grid.txt
+else
+    echo "Error: grid.txt not found."
+    exit 1
+fi
