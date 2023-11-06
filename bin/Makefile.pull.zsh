d10e0ebce3b8a9d2b64469381f6ad9f43cb82471
Stephen Blott <stephen.blott@dcu.ie>
2023-11-06 12:48:59 +0000

    Rework runner script copying, add rust.

---
+++ bin/Makefile.pull.zsh
@@ -0,0 +1,24 @@
+#!/usr/bin/env zsh
+set -e
+dst=$PWD
+
+#
+# The primary copy of some of these scripts is the einstein
+# repo.  This script just copies them across to here, because
+# we need them here too.
+#
+
+markers=(
+  show-exec-command
+  c-marker
+  rust-marker
+)
+
+rsync -ai ~/projects/einstein/user-scripts/runner
+
+(
+  cd ~/projects/einstein/docker/docker-image/res
+  rsync -ai $execs $dst/
+)
+
+chmod a+x runner $execs
